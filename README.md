b03742275349aa689b981a71d15479ee9a222c77
Vincent-vst <louise.insomnia@gmail.com>
2021-07-24 22:06:58 +0100

    centered + readme

--- README.md
+++ README.md
@@ -16,13 +16,14 @@ python main.py
 
 ## Usage
 
-Once you hit your keyboard, the timer will start. 
-Mistakes will appear in red, and once you finish writing, your stats will show off on screen. 
+Once you hit your keyboard, the timer will start.   
+Mistakes will appear in red, and once you finish writing, your stats will show off on screen.   
+All sentences are randomly chosen in the pangram.txt. So if you want to use your own text, you can edit this file. 
 
 ## Contributing
 
-If there is any issue with the above programm, feel free to open a issue so that i can fix it. 
-Pull request are welcome too. 
+If there is any issue with the above programm, feel free to open a issue so that i can fix it.   
+Pull request are welcome too.   
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- main.py
+++ main.py
@@ -7,15 +7,20 @@ typing_text = 'The quick brown fox jumps over the lazy dog'
 def main(stdscr) : 
     curses.start_color()
     curses.use_default_colors()
-    
+ 
+    # get screen size in order to center the text 
+    rows, cols = stdscr.getmaxyx()
+    center_x = int(rows/2)
+    center_y = int(cols/2)
+
     # Set the color-pair (-1 is for having no background)
     for i in range(0, curses.COLORS):
         curses.init_pair(i + 1, i, -1)
 
     # Print the line in red 
-    stdscr.addstr(0,0,typing_text, curses.color_pair(5))
+    stdscr.addstr(1,int((cols - len(typing_text))/2),typing_text, curses.color_pair(5))
     # Print the line in grey
-    stdscr.addstr(1,0,typing_text, curses.color_pair(244))
+    stdscr.addstr(2,int((cols - len(typing_text))/2),typing_text, curses.color_pair(244))
    
     
     # User input    
@@ -25,22 +30,23 @@ def main(stdscr) :
         
         choice = stdscr.getkey()
         if (choice == typing_text[i]) : 
-            stdscr.addstr(1,i,choice)
+            stdscr.addstr(2,int((cols - len(typing_text))/2) + i ,choice)
         else :
             mistakes += 1
-            stdscr.addstr(1,i,choice, curses.color_pair(2))
+            stdscr.addstr(2,int((cols - len(typing_text))/2) + i,choice, curses.color_pair(2))
     end = time.time()
     
     # Print the time 
-    typing_time = "Time : " + str(end-start)[0:4] + " s"
-    stdscr.addstr(3,0,typing_time, curses.color_pair(12))
+    typing_time = "Time : " + str(round(end-start,2)) + "s"
+    stdscr.addstr(4,int((cols - len(typing_time))/2),typing_time, curses.color_pair(12))
 
     # Print the typing speed
-    wpm = len(typing_text.split()) / ((end-start)/60)
-    stdscr.addstr(4,0, "WPM : " + str(wpm)[0:4]+ " wpm", curses.color_pair(12))
+    wpm = "WPM : " + str(round(len(typing_text.split()) / ((end-start)/60),2)) + " wpm"
+    stdscr.addstr(5,int((cols - len (wpm))/2), wpm, curses.color_pair(12))
 
     # Print the accuracy 
-    stdscr.addstr(5,0, "Accuracy : " + str(100 - ((mistakes*100)/len(typing_text)))[0:4] + "%", curses.color_pair(12)) 
+    accuracy = "Accuracy : " + str(round(100 - ((mistakes*100)/len(typing_text)),2)) + "%"
+    stdscr.addstr(6,int((cols - len(accuracy))/2), accuracy , curses.color_pair(12)) 
 
     stdscr.getch()
 
@@ -48,8 +54,4 @@ if __name__ == "__main__" :
     curses.wrapper(main)
 
 
-# TODO : 
-# - add accuracy 
-# - center everything 
-# - maybe change some colors 
-# - possible bug : using [0:4] it's not a good way to truncate the number. 
+
